9eb9d70a2299fa5f5517decba9d4ee9a4e03479d
Blastypede <jesusalbertoaraque415@gmail.com>
2024-04-11 00:47:46 -0400

    arreglo de cors

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from router.router import user
+from fastapi.middleware.cors import CORSMiddleware
 
 #uvicorn main:app --reload (Comando para iniciar el server)
 
 app = FastAPI() #Inicio de la app
 app.include_router(user) #Incluye el enrutador "user" en la aplicación principal
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
