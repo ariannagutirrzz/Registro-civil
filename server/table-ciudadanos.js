c380954c1810c342ccf244b422a95a7c1893e5e1
ariannagutirrzz <ariannavega2021@gmail.com>
2024-04-12 03:00:51 -0400

    boton para modificar ciudadanos

--- server/table-ciudadanos.js
+++ server/table-ciudadanos.js
@@ -1,5 +1,5 @@
 
-const modal = (cedula) => {
+const modalDelete = (cedula) => {
     Swal.fire({
         title: "Are you sure you want to delete this?",
         showDenyButton: true,
@@ -12,11 +12,11 @@ const modal = (cedula) => {
             axios.delete(`http://localhost:8000/Ciudadanos/delete/${cedula}`)
             .then((response) => {
               console.log(response);
-              alert("Registro exitoso");
+              alert("Ciudadano eliminado correctamente");
             })
             .catch((error) => {
               console.error(error);
-              alert("Error al registrar");
+              alert("Error al eliminar un ciudadano");
             });
           Swal.fire("Deleted!", "", "success");
           location.reload();
@@ -26,6 +26,41 @@ const modal = (cedula) => {
         }
       });
 }
+
+// const modalUpdate = (cedula) => {
+//     Swal.fire({
+//         title: "Are you sure you want to update this?",
+//         showDenyButton: true,
+//         showCancelButton: true,
+//         confirmButtonText: "Yes, update it",
+//         denyButtonText: `Don't save`
+//       }).then((result) => {
+//         /* Read more about isConfirmed, isDenied below */
+//         if (result.isConfirmed) {
+//             axios.put(`http://localhost:8000/Ciudadanos/update/${cedula}`)
+//             .then((response) => {
+//               console.log(response);
+//               alert("Ciudadano actualizado correctamente");
+//             })
+//             .catch((error) => {
+//               console.error(error);
+//               alert("Error al actualizar un ciudadano");
+//             });
+//           Swal.fire("Updated!", "", "success");
+//           location.reload();
+
+//         } else if (result.isDenied) {
+//           Swal.fire("Changes are not saved", "", "info");
+//         }
+//       });
+// }
+
+// json = [{
+//     cedula: "123456789",
+//     nacionalidad: "V",
+//     estado_civil: "Soltero",
+//     nacimientos_id: 1
+// }]
 
 document.addEventListener("DOMContentLoaded", () => {
     axios.get("http://localhost:8000/Ciudadanos/read")
@@ -37,7 +72,8 @@ document.addEventListener("DOMContentLoaded", () => {
                         <td>${ciudadano.nacionalidad || "Dato sin registrar"}</td>
                         <td>${ciudadano.estado_civil || "Dato sin registrar"} </td>
                         <td>${ciudadano.nacimientos_id || "Dato sin registrar"}</td>
-                        <td><button onClick="modal(${ciudadano.cedula})" class="back-button">Eliminar</button></td>
+                        <td><button onClick="modalDelete(${ciudadano.cedula})" class="back-button">Eliminar</button></td>
+                        <td></td><button onClick="modalUpdate(${ciudadano.cedula})" class="back-button">Actualizar</button></td>
                     </tr>
                 `;
             }).join("");
