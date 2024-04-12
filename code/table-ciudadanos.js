48f8bfdae27672853ce695f79541db9e59574b95
ariannagutirrzz <ariannavega2021@gmail.com>
2024-04-12 01:05:51 -0400

    estructura para el get

--- code/table-ciudadanos.js
+++ code/table-ciudadanos.js
@@ -19,32 +19,44 @@ const modal = (cedula) => {
       });
 }
 
-const json = [{
-    nombre: "Carlos",
-    cedula: "123456789",
-    sexo: "M",
-    fecha_nacimiento: "2021-10-10",
-},
-{
-    nombre: "Ari",
-    cedula: "30605255",
-    sexo: "F",
-    fecha_nacimiento: "2004-06-08",
-},
-{
-    nombre: "Luis",
-    cedula: "123456789",
-}
-];
+// const json = [{
+//     nombre: "Carlos",
+//     cedula: "123456789",
+//     sexo: "M",
+//     fecha_nacimiento: "2021-10-10",
+// },
+// {
+//     nombre: "Ari",
+//     cedula: "30605255",
+//     sexo: "F",
+//     fecha_nacimiento: "2004-06-08",
+// },
+// {
+//     nombre: "Luis",
+//     cedula: "123456789",
+// }
+// ];
 
-document.getElementById("body").innerHTML = json.map((ciudadano) => {
-    return `
-        <tr>
-            <td>${ciudadano.nombre || "Dato sin registrar"}</td>
-            <td>${ciudadano.cedula || "Dato sin registrar"}</td>
-            <td>${ciudadano.sexo || "Dato sin registrar"} </td>
-            <td>${ciudadano.fecha_nacimiento || "Dato sin registrar"}</td>
-            <td><button onClick="modal(${ciudadano.cedula})" class="back-button">Eliminar</button></td>
-        </tr>
-    `;
-}).join("");
+document.addEventListener("DOMContentLoaded", (e) => {
+    //   const miForm = document.getElementById("form-ciudadanos");
+    axios.get("http://localhost:8000/Ciudadanos/read")
+          .then((response) => {
+            document.getElementById("body").innerHTML = response.data.map((ciudadano) => {
+                return `
+                    <tr>
+                        <td>${ciudadano.cedula || "Dato sin registrar"}</td>
+                        <td>${ciudadano.nacionalidad || "Dato sin registrar"}</td>
+                        <td>${ciudadano.estado_civil || "Dato sin registrar"} </td>
+                        <td>${ciudadano.nacimientos_id || "Dato sin registrar"}</td>
+                        <td><button onClick="modal(${ciudadano.cedula})" class="back-button">Eliminar</button></td>
+                    </tr>
+                `;
+            }).join("");
+            console.log(response);
+            alert("Datos mostrados correctamente");
+          })
+          .catch((error) => {
+            console.error(error);
+            alert("Error al mostrar los datos");
+          });
+      });
