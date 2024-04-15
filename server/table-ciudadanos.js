878800512c9f71b27093f6e3c9d97c889ea696b3
ariannagutirrzz <ariannavega2021@gmail.com>
2024-04-15 02:37:17 -0400

    get eliminado

--- server/table-ciudadanos.js
+++ server/table-ciudadanos.js
@@ -27,43 +27,8 @@ const modalDelete = (cedula) => {
       });
 }
 
-// const modalUpdate = (cedula) => {
-//     Swal.fire({
-//         title: "Are you sure you want to update this?",
-//         showDenyButton: true,
-//         showCancelButton: true,
-//         confirmButtonText: "Yes, update it",
-//         denyButtonText: `Don't save`
-//       }).then((result) => {
-//         /* Read more about isConfirmed, isDenied below */
-//         if (result.isConfirmed) {
-//             axios.put(`http://localhost:8000/Ciudadanos/update/${cedula}`)
-//             .then((response) => {
-//               console.log(response);
-//               alert("Ciudadano actualizado correctamente");
-//             })
-//             .catch((error) => {
-//               console.error(error);
-//               alert("Error al actualizar un ciudadano");
-//             });
-//           Swal.fire("Updated!", "", "success");
-//           location.reload();
-
-//         } else if (result.isDenied) {
-//           Swal.fire("Changes are not saved", "", "info");
-//         }
-//       });
-// }
-
-// json = [{
-//     cedula: "123456789",
-//     nacionalidad: "V",
-//     estado_civil: "Soltero",
-//     nacimientos_id: 1
-// }]
-
 document.addEventListener("DOMContentLoaded", () => {
-    axios.get("http://localhost:8000/Ciudadanos/read")
+  axios.get(`http://localhost:8000/Ciudadanos/read/${cedula}`)
           .then((response) => {
             document.getElementById("body").innerHTML = response.data.map((ciudadano) => {
                 return `
@@ -103,29 +68,3 @@ document.addEventListener("DOMContentLoaded", () => {
           alert("Error al cargar los datos del ciudadano");
       });
 });
-
-document.addEventListener("DOMContentLoaded", () => {
-  axios.get(`http://localhost:8000/Ciudadanos/read/${cedula}`)
-        .then((response) => {
-          document.getElementById("body").innerHTML = response.data.map((ciudadano) => {
-              return `
-                  <tr>
-                      <td>${ciudadano.cedula || "Dato sin registrar"}</td>
-                      <td>${ciudadano.nacionalidad || "Dato sin registrar"}</td>
-                      <td>${ciudadano.estado_civil || "Dato sin registrar"} </td>
-                      <td>${ciudadano.nacimientos_id || "Dato sin registrar"}</td>
-                      <td><button onClick="modalDelete(${ciudadano.cedula})" class="back-button">Eliminar</button></td>
-                      <td>
-                      <a href="../modificar/modificar-ciudadano.html?cedula=${ciudadano.cedula}">Modificar</a>
-                      </td>
-                  </tr>
-              `;
-          }).join("");
-          console.log(response);
-          alert("Datos mostrados correctamente");
-        })
-        .catch((error) => {
-          console.error(error);
-          alert("Error al mostrar los datos");
-        });
-    });
