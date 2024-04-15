c891d378ea2011bf9d046a07de85242873315acc
ariannagutirrzz <ariannavega2021@gmail.com>
2024-04-15 02:48:26 -0400

    params

--- server/table-ciudadanos.js
+++ server/table-ciudadanos.js
@@ -26,30 +26,33 @@ const modalDelete = (cedula) => {
         }
       });
 }
-
 document.addEventListener("DOMContentLoaded", () => {
+  const params = new URLSearchParams(window.location.search);
+  const cedula = params.get('cedula');
+
   axios.get(`http://localhost:8000/Ciudadanos/read/${cedula}`)
-          .then((response) => {
-            document.getElementById("body").innerHTML = response.data.map((ciudadano) => {
-                return `
-                    <tr>
-                        <td>${ciudadano.cedula || "Dato sin registrar"}</td>
-                        <td>${ciudadano.nacionalidad || "Dato sin registrar"}</td>
-                        <td>${ciudadano.estado_civil || "Dato sin registrar"} </td>
-                        <td>${ciudadano.nacimientos_id || "Dato sin registrar"}</td>
-                        <td><button onClick="modalDelete(${ciudadano.cedula})" class="back-button">Eliminar</button></td>
-                        <td><a href="../modificar/modificar-ciudadano.html?cedula=${ciudadano.cedula}">Modificar</a></td>
-                    </tr>
-                `;
-            }).join("");
-            console.log(response);
-            alert("Datos mostrados correctamente");
-          })
-          .catch((error) => {
-            console.error(error);
-            alert("Error al mostrar los datos");
-          });
-      });
+        .then((response) => {
+          document.getElementById("body").innerHTML = response.data.map((ciudadano) => {
+              return `
+                  <tr>
+                      <td>${ciudadano.cedula || "Dato sin registrar"}</td>
+                      <td>${ciudadano.nacionalidad || "Dato sin registrar"}</td>
+                      <td>${ciudadano.estado_civil || "Dato sin registrar"} </td>
+                      <td>${ciudadano.nacimientos_id || "Dato sin registrar"}</td>
+                      <td><button onClick="modalDelete('${ciudadano.cedula}')" class="back-button">Eliminar</button></td>
+                      <td><a href="../modificar/modificar-ciudadano.html?cedula=${ciudadano.cedula}">Modificar</a></td>
+                  </tr>
+              `;
+          }).join("");
+          console.log(response);
+          alert("Datos mostrados correctamente");
+        })
+        .catch((error) => {
+          console.error(error);
+          alert("Error al mostrar los datos");
+        });
+});
+
 
 // Editar formularios
 
