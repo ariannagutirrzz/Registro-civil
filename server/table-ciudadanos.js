ba9aeeef8a062b0cfb8933583cebb17267d48cf5
antoniodmp <antoniodmartinezp03@gmail.com>
2024-04-15 02:39:18 -0400

    Pruebas

--- server/table-ciudadanos.js
+++ server/table-ciudadanos.js
@@ -27,41 +27,6 @@ const modalDelete = (cedula) => {
       });
 }
 
-// const modalUpdate = (cedula) => {
-//     Swal.fire({
-//         title: "Are you sure you want to update this?",
-//         showDenyButton: true,
-//         showCancelButton: true,
-//         confirmButtonText: "Yes, update it",
-//         denyButtonText: `Don't save`
-//       }).then((result) => {
-//         /* Read more about isConfirmed, isDenied below */
-//         if (result.isConfirmed) {
-//             axios.put(`http://localhost:8000/Ciudadanos/update/${cedula}`)
-//             .then((response) => {
-//               console.log(response);
-//               alert("Ciudadano actualizado correctamente");
-//             })
-//             .catch((error) => {
-//               console.error(error);
-//               alert("Error al actualizar un ciudadano");
-//             });
-//           Swal.fire("Updated!", "", "success");
-//           location.reload();
-
-//         } else if (result.isDenied) {
-//           Swal.fire("Changes are not saved", "", "info");
-//         }
-//       });
-// }
-
-// json = [{
-//     cedula: "123456789",
-//     nacionalidad: "V",
-//     estado_civil: "Soltero",
-//     nacimientos_id: 1
-// }]
-
 document.addEventListener("DOMContentLoaded", () => {
     axios.get("http://localhost:8000/Ciudadanos/read")
           .then((response) => {
@@ -73,7 +38,7 @@ document.addEventListener("DOMContentLoaded", () => {
                         <td>${ciudadano.estado_civil || "Dato sin registrar"} </td>
                         <td>${ciudadano.nacimientos_id || "Dato sin registrar"}</td>
                         <td><button onClick="modalDelete(${ciudadano.cedula})" class="back-button">Eliminar</button></td>
-                        // <td><button onClick="modalUpdate(${ciudadano.cedula})" class="back-button">Modificar</button></td>
+                        <td><a href="../modificar/modificar-ciudadano.html?cedula=${ciudadano.cedula}">Modificar</a></td>
                     </tr>
                 `;
             }).join("");
@@ -88,21 +53,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Editar formularios
 
-document.addEventListener("DOMContentLoaded", () => {
-  axios.get("http://localhost:8000/Ciudadanos/read")
-      .then((response) => {
-          // Rellenar los campos del formulario con los datos obtenidos
-          document.getElementById("cedula").value = response.data.cedula;
-          document.getElementById("nacionalidad").value = response.data.nacionalidad;
-          document.getElementById("estado_civil").value = response.data.estado_civil;
+// document.addEventListener("DOMContentLoaded", () => {
+//   axios.get("http://localhost:8000/Ciudadanos/read")
+//       .then((response) => {
+//           // Rellenar los campos del formulario con los datos obtenidos
+//           document.getElementById("cedula").value = response.data.cedula;
+//           document.getElementById("nacionalidad").value = response.data.nacionalidad;
+//           document.getElementById("estado_civil").value = response.data.estado_civil;
 
-          // Rellenar otros campos del formulario
-      })
-      .catch((error) => {
-          console.error(error);
-          alert("Error al cargar los datos del ciudadano");
-      });
-});
+//           // Rellenar otros campos del formulario
+//       })
+//       .catch((error) => {
+//           console.error(error);
+//           alert("Error al cargar los datos del ciudadano");
+//       });
+// });
 
 document.addEventListener("DOMContentLoaded", () => {
   axios.get(`http://localhost:8000/Ciudadanos/read/${cedula}`)
